2f232c63e9b2e32a8dfca7cf04cf27fbcba250ab
Hirisci <www.alan92@icloud.com>
2022-10-06 14:49:16 +0200

    Aggiungere i file di progetto.

---
+++ la-mia-pizzeria-post/Controllers/HomeController.cs
@@ -0,0 +1,71 @@
+﻿using la_mia_pizzeria_post.Data;
+using la_mia_pizzeria_post.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+
+namespace la_mia_pizzeria_model.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly PizzeDbContext _db;
+
+        public HomeController(ILogger<HomeController> logger, PizzeDbContext db)
+        {
+            _db = db;
+            _logger = logger;
+            
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Menu()
+        {
+            List<Pizza> pizze = _db.Pizze.ToList();
+            return View(pizze);
+        }
+        
+        public IActionResult Create()
+        {            
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Pizza pizza)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(pizza);
+            }
+            
+            _db.Pizze.Add(pizza);
+            _db.SaveChanges();
+            return RedirectToAction(nameof(Menu));
+        }
+
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            Pizza? pizza = _db.Pizze.First(x => x.Id == id);
+            return View(pizza);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ la-mia-pizzeria-post/Models/Pizza.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace la_mia_pizzeria_post.Models
+{
+    public class Pizza
+    {
+        public int Id { get; set; }
+        public string Img { get; set; }
+        [StringLength(50, ErrorMessage = "Il nome della pizza non puó superare i {1} caratteri")]
+        [Required]
+        public string Name { get; set; }
+        [StringLength(1000, ErrorMessage = "Il nome della pizza non puó superare i {1} caratteri")]
+        [RegularExpression(@"(\w+\W){4,}\w+",
+         ErrorMessage = "La descrizione deve avere piú di 5 parole")]
+        public string Descrition { get; set; }
+        [Required]
+        [Range(0, 999.99, ErrorMessage = "Il prezzo della pizza deve essere un valore tra {1} e {2}")]
+        public float Amount { get; set; }
+
+    }
+}
